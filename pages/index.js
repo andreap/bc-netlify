250bbe6b551e788c6de01be14284b2b3b84cd95f
Andrea Paraggio <736408+andreap@users.noreply.github.com>
2023-02-05 17:31:03 +0100

    added functions to index js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,48 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Link from "next/link";
 
-export default function Home() {
+export function Pesce({ id, fields }) {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+    <div>
+      {fields?.illustrazione && (
+        <img width="400" height="400" src={fields.illustrazione[0]?.url} />
+      )}
+      <a href={fields.Homepage}>
+        <h2 className="text-lg font-semibold text-slate-500">{fields.Name}</h2>
+      </a>
     </div>
-  )
+  );
+}
+
+export default function Home({ records }) {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold my-8 leading-tight">
+        Tiny House Communities
+      </h1>
+      <Link href="/new" className="bg-slate-500 text-white px-5 py-4 shadow-md">
+        Add a Community
+      </Link>
+      <div className="grid grid-cols-3 grid-gap-3">
+        {records.map((record, key) => (
+          <Pesce key={key} {...record} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export async function getStaticProps(context) {
+  async function retrieveRecords() {
+    const { data } = await axios.get("https://eoinhafbz9xgrwf.m.pipedream.net");
+
+    return data;
+  }
+
+  const records = await retrieveRecords();
+
+  return {
+    props: { records }, // will be passed to the page component as props
+  };
 }
