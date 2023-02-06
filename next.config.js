23ca4f9fa15e8d769f0594a7f17ebb843dbe54ea
Andrea Paraggio <736408+andreap@users.noreply.github.com>
2023-02-06 12:51:39 +0100

    added next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+
+module.exports = {
+    images: {
+        domains: [
+            "dl.airtable.com",
+            "v5.airtableusercontent.com"
+        ],
+        unoptimized: true,
+    },
+  };
