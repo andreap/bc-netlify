d4074f5deae0f8dfcc5682f33d48fd87d3184557
Andrea Paraggio <736408+andreap@users.noreply.github.com>
2023-02-07 10:18:30 +0100

    added logo component

---
+++ components/Logo.js
@@ -0,0 +1,23 @@
+// components/Logo.js
+
+import styles from "../styles/Logo.module.css";
+import Image from "next/legacy/image";
+
+export const Logo = ({ data }) => {
+  return (
+    <div className={styles.logo}>
+      {data[0]?.media?.length && (
+        <div className={styles.logoImg}>
+          <Image
+            layout='intrinsic'
+            src={data[0]?.media[0]?.fields?.url[0]?.url}
+            alt={data[0]?.media[0]?.fields?.alt}
+            objectFit='fill'
+            width={400}
+            height={400}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
